80c0409c0b894c1c0183165b18c8bb30f317a369
May Lee <maylee841012@gmail.com>
2023-07-25 19:02:32 +0800

    ✨ Feat: Setup deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Need to specific file routes for production
+  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/' : '/',
   plugins: [react()],
 })
